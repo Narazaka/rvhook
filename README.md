d1a386ef5cf08e48bddc4ce15c2129a555d1e46f
Nobu <nobu@ctmk.amanek.jp>
2014-03-23 14:28:40 +0900

    add rvhook ver 1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 rvhook
 ======
-
 an application which hooks starting the game of rpgvxace up.
+rvhook: rpgvxaceがGame.exeを起動する際に別の処理をフックするためのツールです。
+
+## 使い方
+rpgvxaceのプロジェクトを作成後、次の作業を行ってください:
+* Game.exeを_Game.exeにリネームする
+* Game.iniを_Game.iniにリネームする
+* rvhookのGame.exeを、作成したプロジェクトのフォルダにコピーする
+* hook.batというファイルを、作成したプロジェクトのフォルダに作成する
+
+## 挙動
+1. rpgvxaceのエディタからゲームを実行しようとするとrvhookのGame.exeが起動します
+2. rvhookのGame.exeはhook.batを実行します。  
+ゲーム起動時に実行したい処理がある場合は、このバッチファイルに記述してください。
+3. rvhookのGame.exeは、_Game.exe(本来のGame.exe)を実行します
+
+# 更新履歴
+* 1.0.0	最初のバージョン
+
---
+++ rvhook/main.cpp
@@ -0,0 +1,113 @@
+﻿/*!
+ * @file
+ */
+#define NOMINMAX
+#include <assert.h>
+#include <stdio.h>
+#include <windows.h>
+#include <string>
+#include <vector>
+
+namespace {
+
+	const char FILENAME_TO_EXECUTE[] = "_Game.exe";
+	const char FILENAME_HOOK_PREBOOT[] = "hook_preboot.bat";
+	const char FILENAME_HOOK_POSTBOOT[] = "hook_postboot.bat";
+
+	void waitForConfirmOnError(int exitcode);
+
+	DWORD bootProcess(const char *filename, char *commandLineArguments);
+
+}	// unnamed namespace
+
+int main(int argc, char *argv[])
+{
+	char * commandLineArguments = "";
+	if (argc > 1) {
+		char * itr = argv[1];
+		char * end = argv[argc-1];
+		for (; itr < end; ++itr) {
+			// replace \0 -> space to use it as command line arguments
+			if (*itr == '\0') { *itr = ' '; }
+		}
+		commandLineArguments = argv[1];
+	}
+	
+	// call a pre-boot hook script
+	{
+		const std::string hookcommand = std::string(FILENAME_HOOK_PREBOOT) + std::string(" ") + std::string(commandLineArguments);
+		std::vector<char> command(hookcommand.length());
+		command.assign(hookcommand.begin(), hookcommand.end());
+		command.push_back('\0');
+
+		bootProcess(NULL, &command[0]);
+	}
+
+	// boot Game.exe
+	const DWORD exitcode = bootProcess(FILENAME_TO_EXECUTE, commandLineArguments);
+
+	// call a post-boot hook script
+	{
+		const std::string hookcommand = std::string(FILENAME_HOOK_POSTBOOT) + std::string(" ") + std::string(commandLineArguments);
+		std::vector<char> command(hookcommand.length());
+		command.assign(hookcommand.begin(), hookcommand.end());
+		command.push_back('\0');
+
+		bootProcess(NULL, &command[0]);
+	}
+
+#ifdef _DEBUG
+	printf("it will exit with exitcode %d\n", exitcode);
+	printf("please push any key.\n");
+	getchar();
+#endif
+
+	return exitcode;
+}
+
+namespace {
+
+	void waitForConfirmOnError(int exitcode)
+	{
+		printf("please push any key.\n");
+		getchar();
+		exit(exitcode);
+	}
+
+	DWORD bootProcess(const char *filename, char *commandLineArguments)
+	{
+		printf("run %s with: %s\n", filename, commandLineArguments);
+
+		STARTUPINFO startupinfo = { 0 };
+		GetStartupInfo(&startupinfo);
+
+		PROCESS_INFORMATION processinfo = { 0 };
+
+		{
+			const BOOL result = CreateProcess(filename, commandLineArguments, NULL, NULL, TRUE, 0, NULL, NULL, &startupinfo, &processinfo);
+			if (result != TRUE) {
+				const DWORD errorcode = GetLastError();
+				printf("failed to CreateProcess(%s) because of errorcode %d.\n", filename, errorcode);
+				waitForConfirmOnError(-1);
+			}
+		}
+		CloseHandle(processinfo.hThread);
+
+		WaitForSingleObject(processinfo.hProcess, INFINITE);
+
+		DWORD exitcode = 0;
+		{
+			const BOOL result = GetExitCodeProcess(processinfo.hProcess, &exitcode);
+			CloseHandle(processinfo.hProcess);
+
+			if (result != TRUE) {
+				const DWORD errorcode = GetLastError();
+				printf("failed to GetExitCodeProcess(%s) because of errorcode %d.\n", filename, errorcode);
+				waitForConfirmOnError(-2);
+			}
+		}
+
+		return exitcode;
+	}
+
+}	// unnamed namespace
