a852776c4d696f1e4f150e87a73792234617fcfc
Narazaka <info@narazaka.net>
2025-02-16 17:56:59 +0900

    fix

--- rvhook/main.cpp
+++ rvhook/main.cpp
@@ -7,6 +7,7 @@
 #include <windows.h>
 #include <string>
 #include <vector>
+#include <filesystem>
 
 namespace {
 
@@ -22,6 +23,16 @@ namespace {
 
 int main(int argc, char *argv[])
 {
+	char* exe_path_str = new char[FILENAME_MAX];
+	DWORD exe_path_len = GetModuleFileName(NULL, exe_path_str, FILENAME_MAX);
+	if (exe_path_len == 0) {
+		printf("failed to GetModuleFileName because of errorcode %d.\n", GetLastError());
+		waitForConfirmOnError(-3);
+	}
+	auto exe_path = std::filesystem::path(exe_path_str);
+	delete[] exe_path_str;
+	std::filesystem::current_path(exe_path.parent_path());
+
 	char * commandLineArguments = "";
 	if (argc > 1) {
 		char * itr = argv[1];
@@ -33,27 +44,29 @@ int main(int argc, char *argv[])
 		commandLineArguments = argv[1];
 	}
 	
+	if (std::filesystem::exists(FILENAME_HOOK_PREBOOT))
 	// call a pre-boot hook script
 	{
-		const std::string hookcommand = std::string(FILENAME_HOOK_PREBOOT) + std::string(" ") + std::string(commandLineArguments);
+		const std::string hookcommand = std::string("/c ") + std::string(FILENAME_HOOK_PREBOOT) + std::string(" ") + std::string(commandLineArguments);
 		std::vector<char> command(hookcommand.length());
 		command.assign(hookcommand.begin(), hookcommand.end());
 		command.push_back('\0');
 
-		bootProcess(NULL, &command[0]);
+		bootProcess("C:\\Windows\\System32\\cmd.exe", &command[0]);
 	}
 
 	// boot Game.exe
 	const DWORD exitcode = bootProcess(FILENAME_TO_EXECUTE, commandLineArguments);
 
+	if (std::filesystem::exists(FILENAME_HOOK_POSTBOOT))
 	// call a post-boot hook script
 	{
-		const std::string hookcommand = std::string(FILENAME_HOOK_POSTBOOT) + std::string(" ") + std::string(commandLineArguments);
+		const std::string hookcommand = std::string("/c ") + std::string(FILENAME_HOOK_POSTBOOT) + std::string(" ") + std::string(commandLineArguments);
 		std::vector<char> command(hookcommand.length());
 		command.assign(hookcommand.begin(), hookcommand.end());
 		command.push_back('\0');
 
-		bootProcess(NULL, &command[0]);
+		bootProcess("C:\\Windows\\System32\\cmd.exe", &command[0]);
 	}
 
 #ifdef _DEBUG
